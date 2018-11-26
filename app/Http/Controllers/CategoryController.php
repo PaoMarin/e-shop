185ded1f315c058e4ae642e0f9ae6e63b5c48a22
PaoMarin <paomg.9614@gmail.com>
2018-11-25 23:08:14 -0600

    Creacion del Controlador Categoria

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categories=Category::orderBy('id','DESC');
+        return view('Category.index',compact('categories')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        return view('Category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request,[ 'description'=>'required', 'father_category'=>'required']);
+        Category::create($request->all());
+        return redirect()->route('category.index')->with('success','Categoria creado satisfactoriamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categories=Category::find($id);
+        return  view('category.show',compact('categories'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $categories=Category::find($id);
+        return view('category.edit',compact('categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)    {
+        //
+        $this->validate($request,['description'=>'required', 'father_category'=>'required']);
+
+        Category::find($id)->update($request->all());
+        return redirect()->route('category.index')->with('success','Categoria actualizado satisfactoriamente');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+         Category::find($id)->delete();
+        return redirect()->route('category.index')->with('success','Categoria eliminado satisfactoriamente');
+    }
+}
