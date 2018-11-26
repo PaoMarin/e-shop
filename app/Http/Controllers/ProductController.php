3cea30bc6899d30a2fe676a44ab15547be6911a2
PaoMarin <paomg.9614@gmail.com>
2018-11-25 23:11:34 -0600

    Creacion del Controlador Producto

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Product;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $products=Product::orderBy('id','DESC');
+        return view('Product.index',compact('products')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        return view('Product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request,[ 'sku'=>'required','name'=>'required','description'=>'required', 'imagen'=>'required', 'stock'=>'required', 'precio'=>'required']);
+        Product::create($request->all());
+        return redirect()->route('product.index')->with('success','Producto creado satisfactoriamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $products=Product::find($id);
+        return  view('product.show',compact('products'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $products=Product::find($id);
+        return view('product.edit',compact('products'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)    {
+        //
+        $this->validate($request,['sku'=>'required','name'=>'required','description'=>'required', 'imagen'=>'required', 'stock'=>'required', 'precio'=>'required']);
+
+        Product::find($id)->update($request->all());
+        return redirect()->route('product.index')->with('success','Producto actualizado satisfactoriamente');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+         Product::find($id)->delete();
+        return redirect()->route('product.index')->with('success','Producto eliminado satisfactoriamente');
+    }
+}
